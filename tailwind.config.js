1661fd60c98d3f4b787a83062a684345fef5fa78
SurajSakhare100 <sakharesuraj10@gmail.com>
2025-03-21 19:20:30 +0530

    tailwind issue solve

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{js,ts,jsx,tsx}"], // Ensure correct path
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  };
+  
