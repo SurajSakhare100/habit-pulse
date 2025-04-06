5ca35820b96ea8023b426e69fbd4750564cdc3ef
SurajSakhare100 <sakharesuraj10@gmail.com>
2025-04-06 20:19:03 +0530

    week issue added

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -9,7 +9,7 @@ export const metadata = {
   title: "Habit Pulse",
   description: "Track your daily habits and improve your lifestyle.",
   icons: {
-    icon: "@/favicon.ico",
+    icon: "/favicon.ico",
     shortcut: "/favicon-32x32.png",
     apple: "/apple-touch-icon.png",
   },
