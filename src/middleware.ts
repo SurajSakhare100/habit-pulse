eb1655d4fd3a60e04e0c3339b30d15a548901bfb
SurajSakhare100 <sakharesuraj10@gmail.com>
2025-04-07 20:58:28 +0530

    head tag added

--- src/middleware.ts
+++ src/middleware.ts
@@ -17,6 +17,6 @@ export default withAuth(
 
 export const config = {
   matcher: [
-    "/((?!api/auth|auth|_next/static|_next/image|favicon.ico|$).*)",
+    "/((?!api/auth|auth|_next/static|_next/image|favicon.ico|sitemap.xml|$).*)", // Excluding sitemap.xml here
   ],
 };
