3313c2e57e26ec7f6c29bb165216a0d2949a0b6d
SurajSakhare100 <sakharesuraj10@gmail.com>
2025-04-08 10:37:00 +0530

    sitemap added

---
+++ next-sitemap.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: 'https://habitpulse.xyz',
+    generateRobotsTxt: true, // ⬅️ generates robots.txt too
+    sitemapSize: 5000,
+    changefreq: 'daily',
+    priority: 0.7,
+  };
+  
